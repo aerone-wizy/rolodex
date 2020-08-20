836ad1d75fb199afdfa0c808ad45ee01d5ba629f
aerone-wizy <aerone@wizy.io>
2020-08-20 10:43:52 +0800

    Updates

---
+++ precache-manifest.526ec5c8fb0be83e950f87088e6b8987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf62f4a179bae8a4812b58efea82eac4",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "07ba761ac092f68cb9a1",
+    "url": "/rolodex/static/css/main.b52ab43c.chunk.css"
+  },
+  {
+    "revision": "3d531751eb8d4242cc4b",
+    "url": "/rolodex/static/js/2.59664341.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.59664341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ba761ac092f68cb9a1",
+    "url": "/rolodex/static/js/main.fa43a10e.chunk.js"
+  },
+  {
+    "revision": "f3ae09b6512c78563077",
+    "url": "/rolodex/static/js/runtime-main.d1e3b417.js"
+  }
+]);
